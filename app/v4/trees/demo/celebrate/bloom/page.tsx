6fb9e3284fa375d5d86fb9f65fc73c43ad764524
skerishKang <mtmsylove@gmail.com>
2026-08-03 03:08:05 +0900

    feat(v4): expose purple bloom graph route

---
+++ app/v4/trees/demo/celebrate/bloom/page.tsx
@@ -0,0 +1,6 @@
+import { V4PurpleBloom } from "@/app/components/v4/V4MilestoneExperiences";
+import "@/app/styles/v4/milestone.css";
+
+export default function V4PurpleBloomPage() {
+  return <V4PurpleBloom />;
+}
