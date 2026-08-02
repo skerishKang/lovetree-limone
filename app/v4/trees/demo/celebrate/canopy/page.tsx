ea0f1b78582c7f228ef20600df888b8609871cd0
skerishKang <mtmsylove@gmail.com>
2026-08-03 03:07:56 +0900

    feat(v4): expose rainbow memory canopy route

---
+++ app/v4/trees/demo/celebrate/canopy/page.tsx
@@ -0,0 +1,6 @@
+import { V4RainbowCanopy } from "@/app/components/v4/V4MilestoneExperiences";
+import "@/app/styles/v4/milestone.css";
+
+export default function V4RainbowCanopyPage() {
+  return <V4RainbowCanopy />;
+}
