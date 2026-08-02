90e26322f2bcff6739940ccc3ed96bb95559522d
skerishKang <mtmsylove@gmail.com>
2026-08-03 03:07:47 +0900

    feat(v4): expose aurora particle heart route

---
+++ app/v4/trees/demo/celebrate/aurora/page.tsx
@@ -0,0 +1,6 @@
+import { V4AuroraHeart } from "@/app/components/v4/V4MilestoneExperiences";
+import "@/app/styles/v4/milestone.css";
+
+export default function V4AuroraHeartPage() {
+  return <V4AuroraHeart />;
+}
