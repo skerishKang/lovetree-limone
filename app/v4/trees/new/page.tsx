777d575fac02edb47eccbd0ba3f3c2e34a1c8c8a
skerishKang <mtmsylove@gmail.com>
2026-08-08 17:08:01 +0900

    refactor(v4): remove superseded intermediate creation route

--- app/v4/trees/new/page.tsx
+++
@@ -1,7 +0,0 @@
-import V4ProductSpineStart from "@/app/components/v4/V4ProductSpineStart";
-import "@/app/styles/v4/onboarding.css";
-import "@/app/styles/email-auth.css";
-
-export default function V4NewTreePage() {
-  return <V4ProductSpineStart />;
-}
