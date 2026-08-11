faf1c637ed21f5190405f89308a5976e03e77f6c
skerishKang <mtmsylove@gmail.com>
2026-08-11 10:35:24 +0900

    feat(design-lab): expose isolated moment orbit carousel route

---
+++ app/design-lab/capabilities/moment-orbit-carousel/page.tsx
@@ -0,0 +1,6 @@
+import MomentOrbitCarouselCandidate from "./MomentOrbitCarouselCandidate";
+import "@/app/styles/moment-orbit-carousel-candidate.css";
+
+export default function MomentOrbitCarouselCapabilityPage() {
+  return <MomentOrbitCarouselCandidate />;
+}
